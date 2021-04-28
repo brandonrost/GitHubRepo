9545e31f43601000f48ca1ad9fad55bb87dc8fab
Brandon Rost <brandon.rost@revature.net>
2021-04-28 02:31:58 -0400

    start project one. add skeleton models/utils/packages/dbsetup

---
+++ ProjectOne/src/main/java/com/revature/dbsetup/HibernateDBSetup.java
@@ -0,0 +1,51 @@
+package com.revature.dbsetup;
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.ReimbursementType;
+import com.revature.models.StatusCode;
+import com.revature.models.User;
+import com.revature.models.UserType;
+import com.revature.util.SessionUtility;
+
+public class HibernateDBSetup {
+	Session session = SessionUtility.getSessionFactory().openSession();
+	
+	Transaction tx = session.beginTransaction(); 
+	
+	UserType employeeType = new UserType("Employee"); 
+	UserType financeManagerType = new UserType ("Finance Manager"); 
+	
+	StatusCode pendingCode = new StatusCode("Pending"); 
+	StatusCode acceptedCode = new StatusCode("Accepted");
+	StatusCode rejectedCode = new StatusCode("Rejected"); 
+	
+	ReimbursementType lodgingType = new ReimbursementType("Lodging");
+	ReimbursementType travelType = new ReimbursementType("Travel");
+	ReimbursementType foodType = new ReimbursementType("Food");
+	ReimbursementType otherType = new ReimbursementType("Other");
+	
+	
+	User newEmployee = new User("username", "password","Bob","Burger","email@email.com", new UserType("Employee")); 
+	User newFinanceManager = new User("username1", "password","Jane","Doe", "email1@email.com", new UserType("Finance Manager"));
+	
+//	session.persist(financeManagerType);
+//	session.save(employeeType); 
+//	
+//	session.save(pendingCode); 
+//	session.save(acceptedCode);
+//	session.save(rejectedCode);
+//	
+//	session.save(lodgingType);
+//	session.save(travelType);
+//	session.save(foodType);
+//	session.save(otherType);
+//	
+//	session.save(newEmployee);
+//	session.save(newFinanceManager);
+//
+//	tx.commit();
+
+}
